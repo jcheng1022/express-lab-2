1fa466c12c3e222aa1359b302d716ec48bfda4ce
Jacky <jacky@Jackys-MBP.fios-router.home>
2022-05-16 17:35:42 -0400

    magic 8 ball express application created

--- server.js
+++ server.js
@@ -20,3 +20,15 @@ app.listen(3000, () => {
 app.get('/tip/:total/:tipPercentage', (req,res) => {
     res.send(` ${req.params.total * (req.params.tipPercentage / 100)}`);
 })
+
+//Magic 8 ball 
+
+
+const responses = 
+["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+
+app.get('/magic/:url', (req,res) => {
+    res.send(`${req.params.url}  <h1> ${responses[Math.floor(Math.random() * responses.length)]}</h1>`);
+})
+
